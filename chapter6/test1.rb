9ae3a4105ba8ca1138366f4c3df20792973a19e6
大塚瑞生 <ootsukamizuki@ootsukasuiseinoMacBook-Air.local>
2025-04-18 23:23:40 +0900

    mac作業の追加chapte6のテスト内容追加

---
+++ chapter6/test1.rb
@@ -0,0 +1,7 @@
+#ハッシュを記載してください。
+
+def chapter06_01
+    {name: 'らんてくん',birthday: '7月8日',tall: 80,weight: 25.6 }
+    
+  end
+  puts chapter06_01
---
+++ chapter6/test2.rb
@@ -0,0 +1,6 @@
+def chapter06_02(runtekun_profile)
+    # コードを記述
+    runtekun_profile[:birthday]
+  end
+  puts chapter06_02({ name: 'らんてくん', birthday: '7月8日', tall: 80, weight: 25.6 })
+  
